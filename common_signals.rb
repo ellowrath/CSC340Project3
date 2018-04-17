682bb780d596bdecb4437736960df0693ba0c40b
Matt Hivner <mh2296@uncw.edu>
2018-04-16 20:48:08 -0400

    outputs .csv files for questions 1.a, and 1.b

--- common_signals.rb
+++ common_signals.rb
@@ -1,6 +1,8 @@
 load('fft.rb')
+require 'csv'
+require 'complex'
+include(FFT)
 class CommonSignals
-  include(FFT)
   include(Math)
   attr_reader :n, :two_pi
 
@@ -15,8 +17,19 @@ class CommonSignals
       exp = 2 * k - 1
       (0...@n).each do |i|
         t = i / @n
-        exp2 = @two_pi * exp * t
-        sum[i] += sin(exp2) / exp
+        sum[i] += sin(@two_pi * exp * t) / exp
+      end
+    end
+    sum
+  end
+
+  def sum_even_func(s)
+    sum = Array.new(@n) { 0 }
+    (1..s).each do |k|
+      exp = 2 * k
+      (0...@n).each do |i|
+        t = i / @n
+        sum[i] += sin(@two_pi * exp * t) / exp
       end
     end
     sum
@@ -24,7 +37,33 @@ class CommonSignals
 end
 
 cs = CommonSignals.new
+CSV.open('odd.csv', 'w') do |csv|
+  csv << cs.sum_odd_func(3)
+  csv << cs.sum_odd_func(10)
+  csv << cs.sum_odd_func(50)
+end
+CSV.open('even.csv', 'w') do |csv|
+  csv << cs.sum_even_func(3)
+  csv << cs.sum_even_func(10)
+  csv << cs.sum_even_func(50)
+end
 
-s_three = []
-s_three = cs.sum_odd_func(3)
-p s_three
+f50 = cs.sum_odd_func(50)
+g50 = cs.sum_even_func(50)
+fast_fourier_transform!(f50)
+fast_fourier_transform!(g50)
+(0...f50.length).each do |i|
+  f50[i] *= f50[i].conjugate
+  f50[i] = f50[i].real
+end
+(0...g50.length).each do |i|
+  g50[i] *= g50[i].conjugate
+  g50[i] = g50[i].real
+end
+CSV.open('odd_psd.csv', 'w') do |csv|
+  csv << f50
+end
+
+CSV.open('even_psd.csv', 'w') do |csv|
+  csv << g50
+end
